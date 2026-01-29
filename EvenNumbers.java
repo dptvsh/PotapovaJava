c6a91784dfa46486802253c6012107d58534b32f
Darina Potapova <potapovade15@gmail.com>
2026-01-29 18:25:32 +0500

    корректировка логики вывода четных чисел

--- EvenNumbers.java
+++ EvenNumbers.java
@@ -1,29 +1,17 @@
 import java.util.Arrays;
-import java.util.ArrayList;
-import java.util.List;
 
 public class EvenNumbers {
     public static void main(String[] agrs) {
         int[] numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9,10};
 
         System.out.println("Исходный массив: " + Arrays.toString(numbers));
-
-        List<String> evenNumbers = new ArrayList<>();
+        System.out.print("Четные числа в массиве: ");
 
         for (int number : numbers) {
             if (number % 2 == 0) {
-                evenNumbers.add(String.valueOf(number));
+                System.out.print(number + " ");
             }
         }
-
-        String result;
-        if (evenNumbers.isEmpty()) {
-            result = "четных чисел нет";
-        } else {
-            result = String.join(", ", evenNumbers);
-        }
-
-        System.out.println("Четные числа в массиве: " + result);
 
     }
 }
